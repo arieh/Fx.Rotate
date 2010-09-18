138f105f4f824d9412af498815687fb79a37804b
unknown <Arieh@.(none)>
2010-09-18 12:33:33 +0200

    JSlinted, David Walshe's tips partialy implemented

--- Source/Fx.Rotate.js
+++ Source/Fx.Rotate.js
@@ -14,61 +14,55 @@ provides: [Fx.Rotate]
 
 ...
 */
-(function (window,undef){
+(function ($,window,undef){
 
 /** 
- * taken from http://www.boogdesign.com/examples/transforms/matrix-calculator.html
+ * Matrix function taken from http://www.boogdesign.com/examples/transforms/matrix-calculator.html
  */
-var deg2radians = Math.PI * 2 / 360
-    , prefix = "";
-
-function getMatrix(deg){
-    rad = deg * deg2radians ;
-    costheta = Math.cos(rad);
-    sintheta = Math.sin(rad);
-
-    a = parseFloat(costheta).toFixed(8);
-    c = parseFloat(-sintheta).toFixed(8);
-    b = parseFloat(sintheta).toFixed(8);
-    d = parseFloat(costheta).toFixed(8);
-    return [a,b,c,d];
-}
-
-
-if (Browser.Engine.gecko) prefix = 'moz';
-if (Browser.Engine.webkit) prefix = 'webkit';
-if (Browser.Engine.presto) prefix = 'o';
 
 Fx.Rotate = new Class({
     Extends : Fx
     , element : null
+    , prefix : (Browser.Engine.gecko) ? 'moz' : (Browser.Engine.webkit) ? 'webkit' : (Browser.Engine.presto) ? 'o' : 'ms'
     , initialize : function(el,options){
         this.element = $(el);
-        this.element.setStyle("-"+prefix+"-transform-origin","center center");
         
-        if (Browser.Engine.trident){
+        //cross-browser - set he rotation to spin around it's center as with IE
+        this.element.setStyle("-"+this.prefix+"-transform-origin","center center");
+        
+        if (Browser.Engine.trident){ //this allows the use of the DOM filter API instead of CSS
             this.element.setStyle("filter","progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand')");
         }
         
         this.parent(options);
     }
-    , start : function(from,to){
-        this.parent(from,to);
-    }
     , set : function (current){
-        if (Browser.Engine.trident) current *=-1;
-        var matrix = getMatrix(current);
+        
+        var matrix = this.getMatrix( ((Browser.Engine.trident) ? (current *-1) : current) ), item;
         
         if (Browser.Engine.trident){
-            this.element.filters.item(0).M11 = matrix[0];
-            this.element.filters.item(0).M12 = matrix[1];
-            this.element.filters.item(0).M21 = matrix[2];
-            this.element.filters.item(0).M22 = matrix[3];
+            item = this.element.filters.item(0);
+            
+            item.M11 = matrix[0];
+            item.M12 = matrix[1];
+            item.M21 = matrix[2];
+            item.M22 = matrix[3];
         }else{
-            this.element.setStyle("-"+prefix+"-transform","matrix("+matrix[0]+","+matrix[1]+","+matrix[2]+", "+matrix[3]+", 0, 0)");
+            this.element.setStyle("-"+this.prefix+"-transform","matrix("+matrix[0]+","+matrix[1]+","+matrix[2]+", "+matrix[3]+", 0, 0)");
         }
+    }
+    , getMatrix : function(deg){    
+        var rad = deg * Math.PI * 180
+            , costheta = Math.cos(rad)
+            , sintheta = Math.sin(rad)
+            , a = parseFloat(costheta).toFixed(8)
+            , c = parseFloat(-sintheta).toFixed(8)
+            , b = parseFloat(sintheta).toFixed(8)
+            , d = parseFloat(costheta).toFixed(8);
+            
+        return [a,b,c,d];
     }
 });
 
-})(this);
+})(document.id,this);
 
